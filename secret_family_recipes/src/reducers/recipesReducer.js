f2c436dab49b237aef6d6d3450b1f22c3bbc67f7
Lynda Santiago <you@example.com>
2020-08-24 15:18:48 -0400

    created redux store, reducers, and actions

---
+++ secret_family_recipes/src/reducers/recipesReducer.js
@@ -0,0 +1,35 @@
+import {
+  LOG_ERROR,
+  UPDATE_RECIPES,
+  MAKING_CHANGES,
+} from "../actions/recipesActions";
+
+const initialState = {
+  recipes: [],
+  message: "",
+  makingChanges: false,
+};
+
+export const recipesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case MAKING_CHANGES:
+      return {
+        ...state,
+        makingChanges: true,
+      };
+    case UPDATE_RECIPES:
+      return {
+        ...state,
+        plants: action.payload,
+        message: "",
+        makingChanges: false,
+      };
+    case LOG_ERROR:
+      return {
+        ...state,
+        message: action.payload,
+      };
+    default:
+      return state;
+  }
+};
---
+++ secret_family_recipes/src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import { recipesReducer } from "./recipesReducer";
+import { accountReducer } from "./accountReducer";
+
+export default combineReducers({
+  recipesReducer,
+  accountReducer,
+});
