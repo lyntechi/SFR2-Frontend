0b72ff1c011027f80b41011587ab85aa6fdfcff5
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-08-24 17:49:52 -0500

    fixed json and import statements

--- secret_family_recipes/src/App.js
+++ secret_family_recipes/src/App.js
@@ -1,15 +1,5 @@
-
 import React from "react";
 import "./App.css";
-
-function App() {
-  return <div className="App">
-
-    
-  </div>;
-
-import React from 'react';
-import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login'
 import Validations from './components/validation/validationSchema';
