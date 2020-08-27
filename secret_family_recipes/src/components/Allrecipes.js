09c2aafb644d7cc2a814864a38208116a750464e
Kae Benton <sonja.k.benton@gmail.com>
2020-08-27 17:59:28 -0400

    Search bar styled.

--- secret_family_recipes/src/components/Allrecipes.js
+++ secret_family_recipes/src/components/Allrecipes.js
@@ -44,6 +44,7 @@ export default function AllRecipes(props) {
 
   return (
     <>
+    <div className='searchContainer'>
         <input
           type="text"
           value={searchTerm}
@@ -51,6 +52,7 @@ export default function AllRecipes(props) {
           onChange={handleChange}
           className="searchBar"
         />
+      </div>
       <div className="filteredRecipes container">
         {filteredList.map((item) => {
             return ( 
